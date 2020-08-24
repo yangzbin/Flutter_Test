b2b9d23315d3fead64e41eedbe09d65bce20e2ed
Pizi-yang <2216488055@qq.com>
2020-08-24 10:10:16 +0800

    修改
    Signed-off-by: Pizi-yang <2216488055@qq.com>

--- lib/httpUtil/NWApi.dart
+++ lib/httpUtil/NWApi.dart
@@ -3,7 +3,7 @@ import 'package:cookie_jar/cookie_jar.dart';
 
 class NWApi{
   static final CookieJar cookieJar = new CookieJar();
-  static final baseApi = 'https://www.xxx.com/areamanager/';
+  static final baseApi = 'https://www.sitopclouds.com/areamanager/';
   static final loginPath = 'user/login';//登录
   static final meterList = 'meter/list';//电表列表
 }
