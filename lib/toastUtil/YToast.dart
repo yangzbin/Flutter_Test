271dbc08186d62ee61df30947ea0776b1674f31a
痞子 <oncwnuGVksjjwbdAlV_3SAzhauOg@git.weixin.qq.com>
2020-10-16 11:47:16 +0800

    封装toast

--- lib/toastUtil/YToast.dart
+++ lib/toastUtil/YToast.dart
@@ -6,11 +6,24 @@ class YToast{
   static showText(String msg){
     _showText(msg);
   }
+  static showTextLong(String msg){
+    _showTextLong(msg);
+  }
+
   static void _showText(String msg){
     Fluttertoast.showToast(
       msg: msg,
       backgroundColor: Color(0xFF028A81),
       textColor: Colors.white,
+    );
+  }
+
+  static void _showTextLong(String msg) {
+    Fluttertoast.showToast(
+      msg: msg,
+      backgroundColor: Color(0xFF028A81),
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_LONG
     );
   }
 }
